912c2e5b46d8af203581e8f86f741d039696d3f5
mhamza219 <mhamza219@gmail.com>
2025-02-22 16:24:06 +0530

    add share link

--- app/models/file_upload.rb
+++ app/models/file_upload.rb
@@ -1,4 +1,18 @@
 class FileUpload < ApplicationRecord
   belongs_to :user
   has_one_attached :file
+
+  # after_create :generate_public_token
+
+
+
+
+
+  private
+
+  def generate_public_token
+    self.public_token = SecureRandom.urlsafe_base64(10)
+  end
+
+
 end
